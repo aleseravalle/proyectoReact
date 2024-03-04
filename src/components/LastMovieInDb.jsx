81c8e3402b5b26dd219036ce094d923c5641f7bb
Celia Gallay <celiagallay@gmail.com>
2024-03-04 18:33:53 -0300

    Last Product

--- src/components/LastMovieInDb.jsx
+++ src/components/LastMovieInDb.jsx
@@ -1,5 +1,31 @@
+import React from 'react'
+
 function LastMovieInDb() {
+
+    const [product, setProduct] = React.useState({})
+    React.useEffect(() => {
+        try {
+            fetch('http://localhost:3035/api/product/')
+                .then(res => res.json())
+                .then(response => {
+                    let lastProduct = response.products[response.products.length - 1];
+                    lastProduct = lastProduct.detail
+                    //console.log(lastProduct)
+                    
+                    fetch(lastProduct)
+                        .then(res => res.json())
+                        .then(data => {
+                            console.log(data)
+                            setProduct(data)
+                        })
+                })
+
+
+        } catch (error) { res.send(error) }
+    }, [])
+
     return (
+
         <div className="col-lg-6 mb-4">
             <div className="card shadow mb-4">
                 <div className="card-header py-3">
@@ -7,10 +33,11 @@ function LastMovieInDb() {
                 </div>
                 <div className="card-body">
                     <div className="text-center">
-                        <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{ "width": "40rem" }} src="/images/mandalorian.jpg" alt=" Star Wars - Mandalorian " />
+                        {product.name}
+                        <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{ "width": "40rem" }} src= {product.image} alt={product.name} />
                     </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa citationem ratione aperiam voluptatum non corporis ratione aperiam voluptatum quae dolorem culpa ratione aperiam voluptatum?</p>
-                    <a className="btn btn-danger" target="_blank" rel="nofollow" href="/">View movie detail</a>
+                    <p>{product.description}</p>
+                    <a className="btn btn-danger" target="_blank" rel="nofollow" href="/">Ver detalle</a>
                 </div>
             </div>
         </div>
